9b0fe4fe721800a59e79bf4da35402b26e3e2ef7
Victor <vctrm67@gmail.com>
2019-11-09 17:08:18 -0500

    Edited comments for clarity

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -6,11 +6,7 @@ import java.util.regex.Pattern;
 
 /**
  * This class provides a shortestPath method for finding routes between two points
- * on the map. Start by using Dijkstra's, and if your code isn't fast enough for your
- * satisfaction (or the autograder), upgrade your implementation by switching it to A*.
- * Your code will probably not be fast enough to pass the autograder unless you use A*.
- * The difference between A* and Dijkstra's is only a couple of lines of code, and boils
- * down to the priority you use to order your vertices.
+ * on the map, using the A* algorithm (see shortestPath method)
  */
 
 
